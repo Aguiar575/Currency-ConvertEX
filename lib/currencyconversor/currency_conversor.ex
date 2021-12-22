08d31620f75fd82423b0a41b74662ea323f12efa
Aguiar575 <aguiarus99arthurus@gmail.com>
2021-12-22 11:31:18 -0300

    validations moved to controller;

--- lib/currencyconversor/currency_conversor.ex
+++ lib/currencyconversor/currency_conversor.ex
@@ -1,78 +1,67 @@
 defmodule  Currencyconversor.Conversor do
 
-  @allow_currencies ["BRL","USD", "EUR", "JPY"]
-
-  @spec convert_to(binary, binary, binary) :: %{error: <<_::128, _::_*96>>}
   def convert_to(from, to, amount) do
-    amount = Float.parse(amount) |> elem(0)
-    cond do
-      amount <= 0 or not is_number(amount) ->
-        %{error: "Amount must be an number bigger than 0"}
-      not is_valid_currency?(from) ->
-        %{error: "Invalid currency: 'from'"}
-      not is_valid_currency?(to) ->
-        %{error: "Invalid currency: 'to'"}
-      String.upcase(from) != "EUR" && String.upcase(to) != "EUR" ->
-        %{error: "invalid currency conversion"}
-      true ->
-        handle_conversion(from, to, amount)
-    end
-  end
-
-  @spec is_valid_currency?(binary) :: boolean
-  defp is_valid_currency?(currency) do
-    String.length(currency) == 3 and Enum.member?(@allow_currencies, String.upcase(currency))
-  end
-
-  @spec handle_conversion(binary, binary, number) :: any
-  def handle_conversion(from, to, amount) do
+    amount_f = Float.parse(amount) |> elem(0)
     case get_conversion() do
       %HTTPoison.Response{status_code: 200, body: body} ->
-        converted = get_converted_amount(from, to , amount, Jason.decode!(body))
-        %{  "from" => from,
-            "to" => to,
-            "amount" => amount,
-            "converted" => converted,
-            "currency_rate" => Jason.decode!(body)["rates"][to]
+        IO.inspect(body)
+        converted = get_converted_amount(from, to , amount_f, Jason.decode!(body))
+        IO.inspect(converted)
+        %{
+          status: :success,
+          transaction: %{  from: from,
+              to: to,
+              amount: :erlang.float_to_binary(amount_f, decimals: 2),
+              converted: :erlang.float_to_binary(converted, decimals: 2),
+              currency_rate: get_currency_rate(to, Jason.decode!(body))
+            }
           }
       %HTTPoison.Response{status_code: _, body: body} ->
-        Jason.decode!(body)
+        %{  status: :error,
+            message: Jason.decode!(body)
+          }
       _ -> %{error: "Error while trying to get conection with API"}
     end
   end
 
-  @spec retun_key :: nil | binary
+  def get_currency_rate(to, body) do
+    if String.upcase(to) == "EUR" do
+      "1.0"
+    else
+      body["rates"][String.upcase(to)] |> :erlang.float_to_binary(decimals: 6)
+    end
+  end
+
   defp retun_key(), do: System.get_env("CURRENCY_API_KEY")
 
   def get_conversion() do
     HTTPoison.get!("http://api.exchangeratesapi.io/v1/latest?access_key=#{retun_key()}&base=EUR&symbols=BRL,USD,JPY")
   end
 
-  @spec get_converted_amount(binary, binary, number, map) :: number | %{error: <<_::128, _::_*96>>}
-  def get_converted_amount(from, to, amount, conversion_body) do
+  def get_converted_amount(from, to, amount_f, conversion_body) do
     case from do
-      "EUR" -> convert_to_euro(to, amount, conversion_body)
-      "BRL" -> convert_value_from_euro(amount, conversion_body["rates"]["BRL"])
-      "USD" -> convert_value_from_euro(amount, conversion_body["rates"]["USD"])
-      "JPY" -> convert_value_from_euro(amount, conversion_body["rates"]["JPY"])
-      _ -> %{error: "Invalid currency"}
+      "EUR" -> convert_to_euro(to, amount_f, conversion_body)
+      "BRL" -> convert_value_from_euro(amount_f, conversion_body["rates"]["BRL"])
+      "USD" -> convert_value_from_euro(amount_f, conversion_body["rates"]["USD"])
+      "JPY" -> convert_value_from_euro(amount_f, conversion_body["rates"]["JPY"])
     end
   end
 
-  @spec convert_to_euro(binary, number, map) :: number | %{error: <<_::128, _::_*96>>}
-  def convert_to_euro(to, amount, conversion_body) do
+  def convert_to_euro(to, amount_f, conversion_body) do
     case to do
-      "BRL" -> convert_value_to_euro(amount, conversion_body["rates"]["BRL"])
-      "USD" -> convert_value_to_euro(amount, conversion_body["rates"]["USD"])
-      "JPY" -> convert_value_to_euro(amount, conversion_body["rates"]["JPY"])
-      "EUR" -> amount
-      _ -> %{error: "Invalid currency"}
+      "BRL" -> convert_value_to_euro(amount_f, conversion_body["rates"]["BRL"])
+      "USD" -> convert_value_to_euro(amount_f, conversion_body["rates"]["USD"])
+      "JPY" -> convert_value_to_euro(amount_f, conversion_body["rates"]["JPY"])
+      "EUR" -> amount_f
     end
   end
 
-  defp convert_value_to_euro(amount, rate), do: (amount * rate) |> Float.round(2)
-  defp convert_value_from_euro(amount, rate) do
+  defp convert_value_to_euro(amount_f, rate) when is_float(amount_f) do
+    (amount_f * rate) |> Float.round(2)
+  end
+
+  defp convert_value_from_euro(amount_f, rate) when is_float(amount_f) do
     unit =  if rate == 1, do: 1, else: 1 / rate
-    (amount * unit) |> Float.round(2)
+    (amount_f * unit) |> Float.round(2)
   end
 end
--- lib/currencyconversor_web/controller/conversor_controller.ex
+++ lib/currencyconversor_web/controller/conversor_controller.ex
@@ -4,6 +4,8 @@ defmodule CurrencyconversorWeb.ConversorController do
   alias Currencyconversor.Transaction
   alias Currencyconversor.Transaction.Transactions
 
+  @allow_currencies ["BRL","USD", "EUR", "JPY"]
+
   def convert(conn, params) do
     cond do
       params["user_id"] in ["", nil] ->
@@ -16,29 +18,89 @@ defmodule CurrencyconversorWeb.ConversorController do
         |> put_status(:bad_request)
         |> json(%{error: "'from' is required"})
 
+      not is_valid_currency?(params["from"]) ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Invalid currency: 'from'"})
+
         params["to"] in ["", nil] ->
         conn
         |> put_status(:bad_request)
         |> json(%{error: "'to' is required"})
+
+      not is_valid_currency?(params["to"]) ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Invalid currency: 'to'"})
 
       params["amount"] in ["", nil] ->
         conn
         |> put_status(:bad_request)
         |> json(%{error: "'amount' is required"})
 
-      true -> convert_to(conn, params)
+      not is_amount_valid?(params["amount"]) ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "'amount' must be a number"})
+
+      not amount_is_bigger_than_zero?(params["amount"]) ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "'amount' must be bigger than zero"})
+
+      String.upcase(params["to"]) != "EUR" and String.upcase(params["from"]) != "EUR" ->
+        conn
+        |> put_status(:bad_request)
+        |> json( %{error: "invalid currency conversion"})
+
+      true -> handle_conversion(conn, params)
 
     end
   end
 
-  defp convert_to(conn, params) do
+  @spec is_amount_valid?(binary) :: boolean
+  def is_amount_valid?(amount) do
+    case Float.parse(amount) do
+      :error ->
+        false
+      _ ->
+        true
+    end
+  end
+
+  def is_valid_currency?(currency) do
+    String.length(currency) == 3 and Enum.member?(@allow_currencies, String.upcase(currency))
+  end
+
+  def amount_is_bigger_than_zero?(amount) do
+    Integer.parse(amount) |> elem(0) > 0
+  end
+
+  defp handle_conversion(conn, params) do
     conversion = Conversor.convert_to(params["from"], params["to"], params["amount"])
-    valid_attrs = %{  conversion_rate: "42",
-                      destination_currency: "some destination_currency",
-                      origin_currency: "some origin_currency",
-                      origin_currency_value: "42",
-                      user_id: "42"}
-    {:ok, %Transactions{} = transactions} = Transaction.create_transactions(valid_attrs)
+    case conversion do
+      %{status: :success, transaction: _} ->
+        conn
+        |> return_conversion(conversion.transaction, params["user_id"])
+      %{status: :error, message: _} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(conversion)
+      _ ->
+        conn
+        |> json(conversion)
+    end
+  end
+
+  defp return_conversion(conn, transaction, user_id) do
+    transactions_map = %{ conversion_rate: transaction.currency_rate,
+                          destination_currency: transaction.to,
+                          origin_currency: transaction.from,
+                          origin_currency_value: transaction.amount,
+                          user_id: user_id
+                        }
+    IO.inspect(transactions_map)
+    {:ok, %Transactions{} = transactions} = Transaction.create_transactions(transactions_map)
     conn
     |> put_status(:ok)
     |> json(%{
@@ -47,7 +109,7 @@ defmodule CurrencyconversorWeb.ConversorController do
       origin_currency: transactions.origin_currency,
       origin_currency_value: transactions.origin_currency_value,
       destination_currency: transactions.destination_currency,
-      destination_currency_value: conversion["converted"],
+      destination_currency_value: transaction.converted,
       conversion_rate: transactions.conversion_rate,
       date_time: transactions.inserted_at
     })
